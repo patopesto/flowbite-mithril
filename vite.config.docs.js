cf0443bd5349573106df89aa3ca71bf6332bd731
Alban Moreon <alban.moreon@protonmail.com>
2024-03-10 14:55:57 -0400

    Refactored build configs and scripts

--- vite.config.docs.js
+++ vite.config.docs.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: resolve(__dirname, "dist"),
+    outDir: resolve(__dirname, "src/docs/static/js"),
     lib: {
       entry: resolve(__dirname, "src/index.js"),
       name: "Flowbite Mithril",
